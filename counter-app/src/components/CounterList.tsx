0e95e5bfc73894d45efbbbeae43e8c289e067727
dennisappiahALX <appiahdennis1998@gmail.com>
2023-01-07 14:18:47 +0000

    counter app with react ts

--- counter-app/src/components/CounterList.tsx
+++ counter-app/src/components/CounterList.tsx
@@ -3,11 +3,10 @@ import { AppContext } from '../context/counterContext'
 import {Types} from '../context/counterReducer'
 
 
+
 const CounterList = () : JSX.Element => {
   const { state, dispatch } = useContext(AppContext);
 
-  
-  
   return (
     <React.Fragment>
       <button onClick={() => {
@@ -19,8 +18,9 @@ const CounterList = () : JSX.Element => {
       </button>
       <ul className='list-none' >
         {state.counters.map(counter => <li key={counter.id}>
-            <button className="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-10 my-4">
-              {counter.value}
+          {/* conditionationally rendering classes utitlities */}
+            <button className={`bg-${counter.value > 0 ? 'yellow' : 'red'}-500 text-white font-bold py-2 px-4 rounded mx-10 my-4`}>
+              {counter.value === 0 ? 'zero' : counter.value}
             </button>
             <button onClick={() => {
              dispatch({
